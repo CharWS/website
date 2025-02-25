f26fbdb1d36fdb1091e169b3c12e5e9b626d2eb2
Char <104176019+CharWS@users.noreply.github.com.>
2025-02-24 20:48:07 -0500

    formatting cleanup

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,20 @@
-(function (){
+(function () {
     const elements = document.getElementsByClassName('lightbox');
     const box = document.createElement('div');
-    Object.assign(box.style, {position: 'fixed',display: 'none',justifyContent: 'center',alignItems: 'center',width: '100%',height: '100%',left: '0',top: '0',backgroundColor: 'rgba(0,0,0,.9)', zIndex: '999'});
+    Object.assign(box.style, { position: 'fixed', display: 'none', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100%', left: '0', top: '0', backgroundColor: 'rgba(0,0,0,.9)', zIndex: '999' });
     const img = document.createElement('img');
-    Object.assign(img.style, {maxWidth: '520px',maxHeight: '520px',cursor: 'zoom-out'});
+    Object.assign(img.style, { maxWidth: '520px', maxHeight: '520px', cursor: 'zoom-out' });
     box.appendChild(img);
     box.addEventListener('click', function () {
         box.style.display = 'none';
     });
     document.body.prepend(box);
-    
-    for(element of elements) {
+
+    for (element of elements) {
         element.addEventListener('click', function (e) {
             box.style.display = 'flex';
             img.setAttribute('src', e.target.getAttribute('src'));
         });
         element.style.cursor = 'zoom-in';
     }
-    })();
+})();
